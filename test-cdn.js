d3a73090484581dc0a24d0ab9af10163e40c3ae6
majid598 <143610202+majid598@users.noreply.github.com>
2024-04-04 12:12:46 +0630

    Update test-cdn.js

--- test-cdn.js
+++ test-cdn.js
@@ -1,8 +1,10 @@
-console.log('hello world')
+console.log("hello world");
 
-const style = document.getElementById("lotu")
-  style.style.backgroundColor = 'red'
- const h2 = document.createElement('h2')
-h2.innerHTML = "Hello world from"
+const style = document.getElementById("lotu");
+style.style.backgroundColor = "red";
+const h2 = document.createElement("h2");
+h2.innerHTML = "Hello world from";
 
-style.appendChild(h2)
+style.appendChild(h2);
+
+localStorage.getItem("data");
