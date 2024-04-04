8da6d44d8c4bea2494488fb84cf22db17273341f
Mr raju <143610202+majid598@users.noreply.github.com>
2024-04-04 13:17:09 +0630

    Update test-cdn.js

--- test-cdn.js
+++ test-cdn.js
@@ -1,3 +1,8 @@
 console.log('hello world')
 
-document.getElementById("lotu").style.backgroundColor = 'red'
+const style = document.getElementById("lotu")
+  style.style.backgroundColor = 'red'
+ const h2 = document.createElement('h2')
+h2.innerHTML = "Hello world from"
+
+style.appendChild(h2)
